50e8da63cce7e25ba47531363b37217040622087
Zohaib <ranjha5054@gmail.com>
2026-07-31 23:11:00 +0500

    utils done , setup routes and now writing controllers

--- server/src/app.js
+++ server/src/app.js
@@ -8,4 +8,18 @@ app.use(express.json({limit: "15kb"}))
 app.use(cookieParser({limit: "15kb"}))
 app.use(cors({origin: process.env.CORS_ORIGIN, credentials: true}))
 
+function globalErrorHandler(err, req, res, next) {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal Server Error"
+
+    res
+    .status(statusCode)
+    .json({
+        message,
+        success: false
+    })
+}
+
+app.use()
+
 export {app}
