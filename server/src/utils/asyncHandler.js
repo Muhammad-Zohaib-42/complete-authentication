a989e7dfa478860797f4a246f87e2007e91ff33d
Zohaib <ranjha5054@gmail.com>
2026-08-03 19:10:19 +0500

    delete server folder

--- server/src/utils/asyncHandler.js
+++
@@ -1,7 +0,0 @@
-function asyncHandler(requestHandler) {
-    return function(req, res, next) {
-        Promise.resolve(requestHandler(req, res, next)).catch(error => next(error))
-    }
-}
-
-export {asyncHandler}
